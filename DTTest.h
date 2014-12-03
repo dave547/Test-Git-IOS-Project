d7ef81737138df3a059e41ef1c877b9a69fe5cc0
ranek <ranek@ipw23.pacificwebworks.com>
2014-12-03 11:25:00 -0700

    added test class

---
+++ DTTest.h
@@ -0,0 +1,13 @@
+//
+//  DTTest.h
+//  Git Test
+//
+//  Created by ranek on 12/3/14.
+//  Copyright (c) 2014 B109. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DTTest : NSObject
+
+@end
---
+++ DTTest.m
@@ -0,0 +1,13 @@
+//
+//  DTTest.m
+//  Git Test
+//
+//  Created by ranek on 12/3/14.
+//  Copyright (c) 2014 B109. All rights reserved.
+//
+
+#import "DTTest.h"
+
+@implementation DTTest
+
+@end
